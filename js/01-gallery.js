00384706b061c2e83c5d82ada592faeff7f56756
Oleksandr <alex.vovchok@gmail.com>
2023-02-04 18:13:59 +0200

    created gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+const imagesMarkup = createImgsMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+
+function createImgsMarkup(galleryItems) {
+return galleryItems.map(({preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}).join('');
+}
+
+
+galleryList.addEventListener('click', onGalleryListClick);
+
+function onGalleryListClick(event) {
+    event.preventDefault();
+
+if(!event.target.classList.contains('gallery__image')) {
+    return;
+}
+
+const currentElement = event.target;
+const parentImage = currentElement.closest('.gallery');
+
+console.log(currentElement.getAttribute("data-source"));
+
+const instance = basicLightbox.create(`
+    <img src="${currentElement.dataset.source}">
+`)
+instance.show()
+};
+
+
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryList = document.querySelector('.gallery');
+const imagesMarkup = createImgsMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+
+function createImgsMarkup(galleryItems) {
+return galleryItems.map(({preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+}).join('');
+};
+
+// galleryList.addEventListener('click', onGalleryListClick);
+
+// function onGalleryListClick(event) {
+//     event.preventDefault();
+
+// // if(!event.target.classList.contains('gallery__image')) {
+// //     return;
+// // }
+
+
+// const currentElement = event.target;
+// const parentImage = currentElement.closest('.gallery');
+
+// console.log(currentElement.getAttribute("data-source"));
+
+// var lightbox = $('.gallery a').simpleLightbox({ /* options */ });
+// lightbox.open();
+// };
+
+
+// let gallery = new SimpleLightbox('.gallery a');
+// gallery.on('show.simplelightbox', function () {
+// 	// Do something…
+// });
+
+let gallery = new SimpleLightbox('.gallery a', { /* options */ });
+gallery.on('show.simplelightbox', function(event) {
+    console.log(event);
+});
+
+
+
+
 console.log(galleryItems);
